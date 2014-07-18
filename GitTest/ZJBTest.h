11bc7fc083ccf993819ce5437ff8c075d4392870
Zachary Bernstein <user@new-host.home>
2014-07-17 22:41:53 -0400

    second commit

---
+++ GitTest/ZJBTest.h
@@ -0,0 +1,13 @@
+//
+//  ZJBTest.h
+//  GitTest
+//
+//  Created by Zachary Bernstein on 7/17/14.
+//  Copyright (c) 2014 Zachary Bernstein. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZJBTest : NSObject
+
+@end
---
+++ GitTest/ZJBTest.m
@@ -0,0 +1,13 @@
+//
+//  ZJBTest.m
+//  GitTest
+//
+//  Created by Zachary Bernstein on 7/17/14.
+//  Copyright (c) 2014 Zachary Bernstein. All rights reserved.
+//
+
+#import "ZJBTest.h"
+
+@implementation ZJBTest
+
+@end
